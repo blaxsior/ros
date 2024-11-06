db44f86c99f3d0370433e290434a5ef4f84d59cc
blaxsior <heejun2798@gmail.com>
2024-11-06 22:23:36 +0900

    study: can.STL을 bumperbot 위에 올리기 실습

--- bumperbot_ws/src/bumperbot_cpp_examples/src/simple_parameter.cpp
+++ bumperbot_ws/src/bumperbot_cpp_examples/src/simple_parameter.cpp
@@ -10,7 +10,7 @@ public:
   SimpleParameter(): Node("simple_parameter") {
     declare_parameter<int>("simple_int_param", 28);
     declare_parameter<std::string>("simple_string_param", "Default value");
-    add_on_set_parameters_callback(std::bind(&SimpleParameter::paramChangeCallback, this, _1));
+    param_callback_handle = add_on_set_parameters_callback(std::bind(&SimpleParameter::paramChangeCallback, this, _1));
   }
 
 private:
